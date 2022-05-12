a1dcdc9fa86c0bfb1e914728a71243edf924eca6
r134x7 <r134x7@gmx.com>
2022-05-13 01:30:10 +1000

    initial setup

---
+++ server.js
@@ -0,0 +1,16 @@
+
+
+// inquirer file needed 
+// inquirer questions will be list selection that does sql functions.
+// view all departments = show * from departments table
+// view all roles = show * from roles table
+// view all employees = show * from employees table
+// add a department/role/employee = insert into x table
+// update an employee role = ...
+// when adding x inquirer has to get person to input string
+
+// bonus stuff is update employee managers:
+// view employees by manager, this probably uses...
+// view employees by department, this probably uses...
+// delete departments etc should be simple
+// view combined salaries of all employees in a department would use SUM()
