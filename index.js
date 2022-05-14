4a5975fe96ac62af3082a13e45166b566a0b43f9
r134x7 <r134x7@gmx.com>
2022-05-14 21:10:54 +1000

    added view results to inquirer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: '',
+      password: 'Greenbatteriesonahorse584#',
       database: 'company_db'
     },
     console.log(`Connected to the company_db database.`)
@@ -28,11 +28,18 @@ function select() {
     .then(function (data) {
         
         if (data.optionSelect === "View All Departments"){
-
+            db.query("SELECT id, __name__ AS department FROM department", (err, results) => console.log(table.getTable(results)));
+            // select();
         } else if (data.optionSelect === "View All Roles"){
-
+            db.query(`SELECT __role__.id, __role__.title, department.__name__ AS department, __role__.salary  
+            FROM department
+            JOIN __role__
+            ON __role__.department_id = department.id;`, (err, results) => console.log(table.getTable(results)));
         } else if (data.optionSelect === "View All Employees"){
-            
+            db.query(`SELECT employee.id, employee.first_name, employee.last_name, __role__.title, department.__name__ AS department, __role__.salary, employee.manager_id AS manager  
+            FROM department, __role__
+            JOIN employee
+            ON employee.role_id = __role__.id;`, (err, results) => console.log(table.getTable(results)));
         } else if (data.optionSelect === "Add Department"){
             
         } else if (data.optionSelect === "Add Role"){
@@ -42,7 +49,10 @@ function select() {
         } else {
             return
         }
+
+        // select();
     })
+    // .then(select());
     
     
 }
